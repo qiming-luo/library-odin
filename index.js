5440bb47c7c300a869bf61b5b323eff514b92b05
luoqiming82 <lowuji22@gmail.com>
2023-03-10 23:51:28 +0800

    change toggle read to ueser input, add js get input logic

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ addNewBookButton.addEventListener('click', () => {
 
 // handle user input, and add to table and lib array
 
+// get user input
+let submitBtn = document.getElementById('submit-form');
+function handleSubmit(event) {
+    event.preventDefault();
+    let titleValue = document.getElementById('title-info').value;
+    let authorValue = document.getElementById('author-info').value;
+    let pagesValue = document.getElementById('pages-info').value;
+    let readStateValue = document.getElementById('read-state').value;
+    console.log(titleValue + authorValue + pagesValue + readStateValue);
+}
+submitBtn.addEventListener('click', handleSubmit);
